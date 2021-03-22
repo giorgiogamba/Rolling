6afc8317d05def3ce82f0e736f8c0aad8eb0fce2
Giorgio Gamba <giorgiogamba97@gmail.com>
2021-03-22 16:35:16 +0100

    inizio_primo_livello

---
+++ Assets/Scripts/ObjectiveController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveController : MonoBehaviour
+{
+    //Setuo GameObject as trigger
+    //Ends the level when triggered
+
+    void OnTriggerEnter(Collider ohter) {
+        Debug.Log("END GAME");
+
+        //Make the game stop
+        Camera.main.GetComponent<TimeBarController>().Completed();
+    }
+}
