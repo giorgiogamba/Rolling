2d4289fd649312b82fcd092cd6709ef3c19dacf0
Giorgio Gamba <giorgiogamba97@gmail.com>
2021-03-26 17:15:17 +0100

    completo 8_order_1

--- Assets/Scripts/ColorsController.cs
+++ Assets/Scripts/ColorsController.cs
@@ -8,7 +8,10 @@ public class ColorsController : MonoBehaviour
 
     public List<Image> colors; // a ogni esecuzione deve essere inizializzato in maniera random
     public List<GameObject> objs;
-    int index = 0;
+    public TimeBarController tbc;
+    private int index = 0;
+    private int correct = 0;
+    private int colors_size = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -20,16 +23,16 @@ public class ColorsController : MonoBehaviour
             mat.color = img.color;
             i++;
         }
+        colors_size = colors.Count;
+        tbc = GetComponent<TimeBarController>();
     }
 
     public void CheckColor(Material mat) {
         if (mat.color == colors[index].color)
         {
-            //changing objs color
-            mat.color = Color.yellow;
-
-            //Updating order index
-            index++;
+            mat.color = Color.yellow; //changing objs color
+            index++; //Updating order index
+            correct ++; //updating number of correct colors
         }
         else {
             Debug.Log("SBAGLIATO");
@@ -39,6 +42,8 @@ public class ColorsController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-            
+        if (correct == colors_size) {
+            tbc.Completed();
+        }
     }
 }
